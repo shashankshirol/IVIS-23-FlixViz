c64845a9247c2959cd4d997b5714d0e80b733ef5
Shashank Shirol <shirol@kth.se>
2023-02-10 16:53:56 +0100

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import yaml
 
-with open("keys.yaml") as f:
-    keys = yaml.load(f, Loader=yaml.FullLoader)
+st.title("Flix-Viz")
+st.subheader("Visualize Geo-blocked Netflix Content")
