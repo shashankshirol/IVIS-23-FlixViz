d0859e42a68059424b9e98905c62ed77be9b5030
Nico <naico0028@gmail.com>
2023-02-27 12:13:06 +0100

    minor changes

--- src/table.js
+++ src/table.js
@@ -4,6 +4,7 @@ function table(data) {
 
   data.map((movie, id) => {
     if( id > 10)return
+
     let tr = document.createElement("tr");
     tr.innerHTML = `
         <td><img class="poster" src="${movie.img}"></img></td>
@@ -13,7 +14,7 @@ function table(data) {
         <td>${movie.imdb_rating == null ? "No data" : movie.imdb_rating}</td>
         <td>${movie.votes}</td>
         <td>${movie.genre}</td>
-        <td><a href="https://www.netflix.com/browse?jbv=${movie.nfid}">Netflix</a>${(movie.imdbid != null && movie.imdbid != undefined) ?  ` <a href="https://www.imdb.com/title/${movie.imdbid}">Imdb</a>` : ""}</td>
+        <td><a href="https://www.netflix.com/browse?jbv=${movie.nfid}">Netflix</a>${(movie.imdbid != null && movie.votes != 0) ?  ` <a href="https://www.imdb.com/title/${movie.imdbid}">Imdb</a>` : ""}</td>
         `;
     tbody.append(tr)
   });
