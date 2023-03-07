ad8c714f94e69b08917d9445525cb8e560184944
Nico <naico0028@gmail.com>
2023-03-07 15:15:48 +0100

    selection

--- src/movieComponent.js
+++ src/movieComponent.js
@@ -1,16 +1,24 @@
-function createMovieRow(data){
+function createMovieRow(data, selected){
 let parent = document.getElementById("movieRow");
 parent.innerHTML = "";
 
+let rowData = (selected.length == 0 ? data : selected)
+if(selected.length != 0){
+  selected.map(x =>{
+console.log(x)
+    data = data.filter(y => y.id !== x.id)
+    data.unshift(x)
+  })
+}
 
 
 data.map((movie, id) => {
 if( data.length > 500)
 if( id > 10)return
 let child = document.createElement("div");
-
+if(selected.length!=0 && id < selected.length)
 child.innerHTML = `
-      <div class="wrap">
+      <div id=${id}  class="wrap selected" >
       <img src="${movie.img} alt="movieImage" class="row_poster" />
       <div class="poster_info">
           <div class="poster_text">
@@ -20,6 +28,19 @@ child.innerHTML = `
       </div>
     </div>
   `;
+else{
+child.innerHTML = `
+      <div id=${id} class="wrap selected" >
+      <img src="${movie.img} alt="movieImage" class="row_poster" />
+      <div class="poster_info">
+          <div class="poster_text">
+              <h4>${movie.title}</h4>
+              <p>${movie.synopsis}</p>
+          </div>
+      </div>
+    </div>
+  `;
+}
   parent.append(child)
 
 });
--- src/scatterPlot.js
+++ src/scatterPlot.js
@@ -7,156 +7,187 @@ let svg = d3
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
- d3.json("../Data/data_netflix.json")
-  .then(function(data) {
-    
+d3.json("../Data/data_netflix.json")
+  .then(function (data) {
     const selections = {
-      "year": ["year",1940, 2023, "Year"],
-      "imdb_rating": ["imdb_rating",0,10, "Imdb Rating"],
-      "votes": ["votes",0, 3000000, "Votes"]
-    } 
-    let currX = selections.year
-    let currY = selections.imdb_rating
-    
+      year: ["year", 1940, 2024, "Year"],
+      imdb_rating: ["imdb_rating", 0, 10, "Imdb Rating"],
+      votes: ["votes", 0, 3000000, "Votes"],
+    };
+    let currX = selections.year;
+    let currY = selections.imdb_rating;
+    let selected = [];
+    let countryData = data.filter((d) => d.clist.includes("United States"));
+    data = countryData;
+    createMovieRow(data, selected);
+    // Define the x and y scales
+    let x = d3.scaleLinear().domain([currX[1], currX[2]]).range([0, width]);
+    let y = d3.scaleLinear().domain([currY[1], currY[2]]).range([height, 0]);
+    let xAxis = svg
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-  let countryData = data.filter(d => d.clist.includes("United States"))
-  data.forEach(element => {
-    if(element.imdbid != null && (element.imdbid.split(" | ").length > 1)){
-    console.log(element)
+    let yAxis = svg.append("g").call(d3.axisLeft(y));
+
+    svg
+      .append("text")
+      .attr("text-anchor", "end")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -margin.left + 20)
+      .attr("x", -margin.top)
+      .text(currY[3]);
+
+    svg
+      .append("text")
+      .attr("text-anchor", "end")
+      .attr("x", width)
+      .attr("y", height + margin.top + 20)
+      .text(currX[3]);
+
+    let clip = svg
+      .append("defs")
+      .append("svg:clipPath")
+      .attr("id", "clip")
+      .append("svg:rect")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("x", 0)
+      .attr("y", 0);
+
+    //Interaction functions
+
+    function mouseover() {
+      d3.select(this).attr("r", 8).style("opacity", 1);
     }
-  });
-  data = countryData
-  createMovieRow(data)
-  // Define the x and y scales 
-  let x = d3.scaleLinear().domain([currX[1], currX[2]]).range([0, width]);
-  let y = d3
-    .scaleLinear()
-    .domain([currY[1], currY[2]])
-    .range([height, 0]);
-  let xAxis = svg
-    .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
-
-
-    svg.append("text")
-    .attr("text-anchor", "end")
-    .attr("transform", "rotate(-90)")
-    .attr("y", -margin.left+20)
-    .attr("x", -margin.top)
-    .text(currY[3])
-
-
-
-    svg.append("text")
-    .attr("text-anchor", "end")
-    .attr("x", width)
-    .attr("y", height + margin.top + 20)
-    .text(currX[3])
-
-
-
-
-
-
-  let clip = svg
-    .append("defs")
-    .append("svg:clipPath")
-    .attr("id", "clip")
-    .append("svg:rect")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("x", 0)
-    .attr("y", 0);
-  function mouseover(){
-    d3.select(this).style("fill", "black").attr("stroke", "white").attr("stroke-width", 1.5).transition()
-    .duration('100')
-    .attr("r", 8)
-    .style("opacity", 0.7);
-  }
-  function mouseout(){
-    d3.select(this).style("fill", "red").attr("stroke-width", 0).transition()
-    .duration('200')
-    .attr("r", 6).style("opacity", 0.5);
-  }
-  let yAxis = svg.append("g").call(d3.axisLeft(y));;
-  let brush = d3
-    .brush() // Add the brush feature using the d3.brush function
-    .extent([
-      [0, 0],
-      [width, height],
-    ]) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
-    .on("end", updateChart)
-
-    
-
-
-  // Create the scatter variable: where both the circles and the brush take place
-  let scatter = svg.append("g").attr("clip-path", "url(#clip)");
-scatter
- .append("g")
-   .attr("class", "brush")
-   .call(brush);
- // Add circles
- scatter
- .selectAll("circle")
- .data(data)
- .enter()
- .append("circle")
-   .attr("cx", function (d) { return x(d[currX[0]]); } )
-   .attr("cy", function (d) { return y(d[currY[0]]); } )
-   .attr("r", 6)
-   .style("opacity", 0.5)
-   .style("fill", "#FB0C0C")
-   .attr("pointer-events", "all")
-   .on("mouseover", mouseover)
-   .on("mouseout", mouseout)
-
-  let idleTimeout;
-
-  function idled() { idleTimeout = null; }
- 
-  function updateChart() {
-    let s = d3.event.selection;
-    // If no selection, back to initial coordinate. Otherwise, update X axis domain
-    if (!s) {
-      if (!idleTimeout) return (idleTimeout = setTimeout(idled, 350)); // This allows to wait a little bit
-      x.domain([currX[1],currX[2]])
-      y.domain([currY[1], currY[2]])
-      data = countryData
-    } else { 
-        data = data.filter(d => {
-            let xVals =  [s[0][0], s[1][0]].map(x.invert, x)
-            let yVals = [s[1][1], s[0][1]].map(y.invert, y)
-            if(d[currX[0]]< xVals[1] && d[currX[0]]> xVals[0] && d[currY[0]]< yVals[1] && d[currY[0]]> yVals[0])
-            return d
-        })
-        x.domain([s[0][0], s[1][0]].map(x.invert, x));
-        y.domain([s[1][1], s[0][1]].map(y.invert, y));
-        scatter.select(".brush").call(brush.move, null);
-        
-     // This remove the grey brush area as soon as the selection has been done
+    function mouseout() {
+      d3.select(this).attr("r", 6).style("opacity", 0.5);
     }
-    createMovieRow(data)
-    // Update axis and circle position
-    xAxis.transition().duration(1000).call(d3.axisBottom(x));
-    yAxis.transition().duration(1000).call(d3.axisLeft(y));
+    function displaySelected(d) {
+      if (selected.includes(d)) {
+        console.log("hello");
+        d3.select(this)
+          .style("fill", "red")
+          .attr("stroke-width", 0)
+          .transition()
+          .duration("200")
+          .attr("r", 6)
+          .style("opacity", 0.5);
+
+        selected.splice(
+          selected.findIndex((e) => e.id === d.id),
+          1
+        );
+      } else if (selected.length < 5) {
+        d3.select(this)
+          .style("fill", "black")
+          .attr("stroke", "white")
+          .attr("stroke-width", 1.5)
+          .transition()
+          .duration("100")
+          .attr("r", 8)
+          .style("opacity", 1);
+
+        selected.push(d);
+      } else {
+        return;
+      }
+
+      createMovieRow(data, selected);
+    }
+    function resetCircles() {
+      d3.selectAll("circle")
+        .style("fill", "red")
+        .attr("stroke-width", 0)
+        .transition()
+        .duration("200")
+        .attr("r", 6)
+        .style("opacity", 0.5);
+    }
+
+    let brush = d3
+      .brush() // Add the brush feature using the d3.brush function
+      .extent([
+        [0, 0],
+        [width, height],
+      ]) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
+      .on("end", updateChart);
+
+    // Create the scatter variable: where both the circles and the brush take place
+    let scatter = svg.append("g").attr("clip-path", "url(#clip)");
+    scatter.append("g").attr("class", "brush").call(brush);
+    // Add circles
     scatter
       .selectAll("circle")
-      .transition()
-      .duration(1000)
+      .data(data)
+      .enter()
+      .append("circle")
       .attr("cx", function (d) {
         return x(d[currX[0]]);
       })
       .attr("cy", function (d) {
         return y(d[currY[0]]);
       })
-  }
-})
-.catch(function(error) {
-  console.error(error)
-});
+      .attr("r", 6)
+      .style("opacity", 0.5)
+      .style("fill", "#FB0C0C")
+      .attr("pointer-events", "all")
+      .on("mouseover", mouseover)
+      .on("mouseout", mouseout)
+      .on("click", displaySelected);
+
+    let idleTimeout;
+
+    function idled() {
+      idleTimeout = null;
+    }
+
+    function updateChart() {
+      let s = d3.event.selection;
+      // If no selection, back to initial coordinate. Otherwise, update X axis domain
+      if (!s) {
+        if (!idleTimeout) return (idleTimeout = setTimeout(idled, 350)); // This allows to wait a little bit
+        x.domain([currX[1], currX[2]]);
+        y.domain([currY[1], currY[2]]);
+        data = countryData;
+      } else {
+        data = data.filter((d) => {
+          let xVals = [s[0][0], s[1][0]].map(x.invert, x);
+          let yVals = [s[1][1], s[0][1]].map(y.invert, y);
+          if (
+            d[currX[0]] < xVals[1] &&
+            d[currX[0]] > xVals[0] &&
+            d[currY[0]] < yVals[1] &&
+            d[currY[0]] > yVals[0]
+          )
+            return d;
+        });
+        x.domain([s[0][0], s[1][0]].map(x.invert, x));
+        y.domain([s[1][1], s[0][1]].map(y.invert, y));
+        scatter.select(".brush").call(brush.move, null);
+
+        // This remove the grey brush area as soon as the selection has been done
+      }
+      selected = [];
+      resetCircles()
+      createMovieRow(data, selected);
+      // Update axis and circle position
+      xAxis.transition().duration(1000).call(d3.axisBottom(x));
+      yAxis.transition().duration(1000).call(d3.axisLeft(y));
+      scatter
+        .selectAll("circle")
+        .transition()
+        .duration(1000)
+        .attr("cx", function (d) {
+          return x(d[currX[0]]);
+        })
+        .attr("cy", function (d) {
+          return y(d[currY[0]]);
+        });
+    }
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
